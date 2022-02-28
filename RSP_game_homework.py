51ba278e0fc198bbbe7541e4d0873c2d9e50cd11
Isabel Tulloch <isabel.tulloch@sky.uk>
2022-02-28 19:50:36 +0000

    Update from Monday's Call

--- RSP_game_homework.py
+++ RSP_game_homework.py
@@ -6,8 +6,9 @@ comp_wins = 0
 player_wins = 0
 
 # function to call options
-def Choose_Option():
-    player_choice = input("Choose Rock (R), Paper (P) or Scissors (S): ")
+def choose_option():
+    # .upper so that the comparison is limited to just caps.
+    player_choice = input("Choose Rock (R), Paper (P) or Scissors (S): ").upper()
     if player_choice in ["Rock", "rock", "r", "R"]:
         # user can type in any of the above
         player_choice = "r"
@@ -20,13 +21,13 @@ def Choose_Option():
         # if user doesn't correctly select out of selections then message will appear
         print("Invalid input, please try again.")
         # message will show if user input does not match any of the above
-        Choose_Option()
+        choose_option()
     return player_choice
 # return defines the function, above function will return the players choice, depending on user input
 
 
 # now to define computers actiona/options
-def Computer_Option():
+def computer_option():
     comp_choice = random.randint(1, 3)
     # random integer selection between 1 and 3, 1, 2, 3
     if comp_choice == 1:
@@ -46,8 +47,8 @@ while True:
     # not sure why these were added, ask
     # apparently for spaces?
 
-    player_choice = Choose_Option()
-    comp_choice = Computer_Option()
+    player_choice = choose_option()
+    comp_choice = computer_option()
 
     # defining functions as varaibles - easy to call now
 
@@ -90,8 +91,7 @@ while True:
         elif comp_choice == "s":
             print("Player1: Scissors\nComputer: Scissors\n ...You tied.")
 
-    print("")
-    print("Player wins: " + str(player_wins))
+    print("\nPlayer wins: " + str(player_wins))
     # add values of scores, calcalution to add scores
     print("Computer wins: " + str(comp_wins))
     # not sure why it was converted to a string also, ask why
